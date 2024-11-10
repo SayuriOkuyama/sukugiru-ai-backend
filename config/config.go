9de8391cc90eb3d780069ba3f5f08cdea6b8b880
SayuriOkuyama <mosyaneko77@gmail.com>
2024-11-10 14:29:15 +0900

    vercel、PostgreSQLの設定を追加

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import "os"
+
+type Config struct {
+	Server struct {
+		Port                  string
+		RedirectURLAfterLogin string
+	}
+	Database struct {
+		Host     string
+		User     string
+		Password string
+		Database string
+		Port     string
+	}
+	GeminiAPIKey  string
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+
+	current := &cfg
+
+	current.Server.Port = os.Getenv("SERVER_PORT")
+	// フロントの URI
+	current.Server.RedirectURLAfterLogin = os.Getenv("SERVER_REDIRECT_URL_AFTER_LOGIN")
+	// 追加
+	cfg.GeminiAPIKey = os.Getenv("GEMINI_API_KEY")
+
+	// PostgreSQL
+	current.Database.Host = os.Getenv("POSTGRES_HOST")
+	current.Database.User = os.Getenv("POSTGRES_USER")
+	current.Database.Password = os.Getenv("POSTGRES_PASSWORD")
+	current.Database.Database = os.Getenv("POSTGRES_DATABASE")
+
+	return current, nil
+}
+
+
+
--- main.go
+++ main.go
@@ -4,18 +4,12 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/SayuriOkuyama/sukugiru-ai-backend/config"
+	"github.com/SayuriOkuyama/sukugiru-ai-backend/interactor"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
-
-type Config struct {
-	Server struct {
-		Port                  string
-		RedirectURLAfterLogin string
-	}
-	GeminiAPIKey  string
-}
 
 func main() {
 	// 環境変数取得
@@ -26,13 +20,14 @@ func main() {
 
 	e.Use(middleware.Logger())
 
-	var cfg Config
-	current := &cfg
-	current.Server.Port = os.Getenv("SERVER_PORT")
-	// フロントの URI
-	current.Server.RedirectURLAfterLogin = os.Getenv("SERVER_REDIRECT_URL_AFTER_LOGIN")
-	// 追加
-	cfg.GeminiAPIKey = os.Getenv("GEMINI_API_KEY")
+	cfg, err := config.NewConfig()
+	if err != nil {
+		e.Logger.Error("config error")
+
+		return
+	}
+
+	conn := config.NewDBConnection(cfg)
 
 	// 本番環境以外の場合はデバッグモードを有効にする
 	isDebug := env == "local" || env == "dev" || env == "qa"
@@ -51,14 +46,22 @@ func main() {
 		// ローカル環境検証用：トークンを払い出す
 		// e.GET("/token/:userId", h.GetToken)
 	} else {
-		// 本番環境の場合は CORS を許可する
 		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 			AllowCredentials: true,
 		}))
 	}
 
+	i := interactor.NewInteractor(
+		conn,
+		cfg.GeminiAPIKey,
+	)
+	h := i.NewAppHandler()
+
 	e.GET("/", func(c echo.Context) error {
 			return c.String(http.StatusOK, "ʕ◔ϖ◔ʔ")
+	})
+	e.POST("/contest/prototyping1/originaltheme/generate",func(c echo.Context) error {
+		return h.PostQuestionnaireForTheme(c, false)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
